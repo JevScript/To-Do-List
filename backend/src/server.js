4d4d63d32326a22eb4957359d5d4f3fbb2db03bb
Jean Vicente <jeanvicente2002@gmail.com>
2026-05-12 11:57:35 -0300

    Iniciando criacao das rotas

---
+++ backend/src/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const {PrismaClient} = require('@prisma/client');
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(cors());
+app.use(express.json());
